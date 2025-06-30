4c7b0fe08c2193c95c813281664db83ad143f033
5070 <junst350@gmail.com>
2025-06-30 11:28:11 -0600

    Se Reclutan fundadores

---
+++ album.js
@@ -0,0 +1,68 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      isOpenedTop: true,
+      items: [
+        {
+          img1: "images/avengers1.jpg",
+          img2: "images/avengers2.jpg",
+          img3: "images/avengers3.jpg",
+          title: "Avengers",
+          isOpen: false,
+        },
+        {
+          img1: "images/iron1.jpg",
+          img2: "images/iron2.jpg",
+          img3: "images/iron3.jpg",
+          title: "Iron Man",
+          isOpen: false,
+        },
+        {
+          img1: "images/thor1.jpg",
+          img2: "images/thor2.jpg",
+          img3: "images/thor3.jpg",
+          title: "Thor",
+          isOpen: false,
+        },
+        {
+          img1: "images/hulk1.jpg",
+          img2: "images/hulk2.jpg",
+          img3: "images/hulk3.jpg",
+          title: "Hulk",
+          isOpen: false,
+        },
+        {
+          img1: "images/wasp1.jpg",
+          img2: "images/wasp2.jpg",
+          img3: "images/wasp3.jpg",
+          title: "Wasp",
+          isOpen: false,
+        },
+        {
+          img1: "images/antman1.jpg",
+          img2: "images/antman2.jpg",
+          img3: "images/antman3.jpg",
+          title: "Ant-Man",
+          isOpen: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    topOpen() {
+      this.isOpenedTop = !this.isOpenedTop;
+    },
+
+    open(idx, isOpen) {
+      if (this.isOpenedTop) {
+        this.items[idx].isOpen = !isOpen;
+      }
+    },
+
+    reset() {
+      this.items.forEach((item) => (item.isOpen = false));
+      this.isOpenedTop = false;
+    },
+  },
+});
