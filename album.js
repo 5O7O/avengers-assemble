d40a2d2b57d96fea741c434a1ffa331d58fef397
5070 <junst350@gmail.com>
2025-06-30 11:59:39 -0600

    !Chasquido de Thanos¡ v2

--- album.js
+++ album.js
@@ -12,40 +12,16 @@ new Vue({
           isOpen: false,
         },
         {
-          img1: "images/iron1.jpg",
-          img2: "images/iron2.jpg",
-          img3: "images/iron3.jpg",
-          title: "Iron Man",
-          isOpen: false,
-        },
-        {
           img1: "images/new1.jpg",
           img2: "images/new2.jpg",
           img3: "images/new3.jpg",
           title: "New Avengers",
           isOpen: false,
         },
-        {
+        
 <<<<<<< HEAD
 <<<<<<< HEAD
-          img1: "images/cap1.jpg",
-          img2: "images/cap2.jpg",
-          img3: "images/cap3.jpg",
-          title: "Captain America",
-=======
-          img1: "images/widow1.jpg",
-          img2: "images/widow2.jpg",
-          img3: "images/widow3.jpg",
-          title: "Black Widow",
->>>>>>> feature/viuda-negra
-=======
-          img1: "images/panter1.jpg",
-          img2: "images/panter2.jpg",
-          img3: "images/panter3.jpg",
-          title: "Black Panter",
->>>>>>> feature/pantera-negra
-          isOpen: false,
-        },
+         
       ],
     };
   },
